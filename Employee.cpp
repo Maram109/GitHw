f9ee6f6663ff12917745c61b343f5806f96ab552
unknown <manalab@ac.sce.ac.il>
2019-12-22 15:54:30 +0200

    employee

--- Employee.cpp
+++ Employee.cpp
@@ -1,26 +1,1 @@
-#pragma once
-#include "Person.h"
-#include "Employee.h"
-#include <iostream>
-using namespace std;
-Employee::Employee() {
-	char name[10] = { "" };
-	long id = 0;
-	int age = 0;
-	float salary = 0.0;
-}
-Employee::Employee(char* n, long i, int a, float s):Person(n, i, a) {
-	salary = s;
-}
-void Employee::setSalary(float s) {
-	salary = s;
-}
-float Employee::geSalary() {
-	return salary;
-}
-
-void Employee::Print()const
-{
-	Person::Print();
-	cout << "Salary" << salary << endl;
-}
+#pragma once#include "Person.h"#include "Person.h"#include "Employee.h"#include <iostream>using namespace std;Employee::Employee() {	char name[10] = { "" };	long id = 0;	int age = 0;	float salary = 0.0;}Employee::Employee(char* n, long i, int a, float s):Person(n, i, a) {	salary = s;}void Employee::setSalary(float s) {	salary = s;}float Employee::geSalary() {	return salary;}void Employee::Print()const{	Person::Print();	cout << "Salary" << salary << endl;}
--- Employee.h
+++ Employee.h
@@ -1,15 +1,1 @@
-#pragma once
-#include "Person.h"
-class Employee : virtual public Person {
-private:
-	float salary;
-public:
-
-	Employee();
-	Employee(char*, long, int, float);
-
-	void setSalary(float);
-	float geSalary();
-
-	void Print()const;
-};
+#pragma once#include "Person.h"#include "Person.h"class Employee : virtual public Person {private:	float salary;public:	Employee();	Employee(char*, long, int, float);	void setSalary(float);	float geSalary();	void Print()const;};
